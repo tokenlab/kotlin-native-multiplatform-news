7140632d1cb3e605e1c2d8494e76a0a4be30d444
Daniele Boscolo <danielehidalgo@gmail.com>
2018-04-19 16:06:47 -0300

    Remove Couchbase from iOS project

--- applications/ios/KotlinDemoApp/Store/Data Source/Local/ArticleLocalDataSource.swift
+++ applications/ios/KotlinDemoApp/Store/Data Source/Local/ArticleLocalDataSource.swift
@@ -8,65 +8,18 @@
 
 import Foundation
 import Common
-import CouchbaseLite
-import CouchbaseLiteSwift
 
 class ArticleLocalDataSource: NSObject {
     static let databaseName: String = "article-database"
-    
-    var database: Database!
-    
-    override init() {
-        self.database = try! Database(name: ArticleLocalDataSource.databaseName)
-    }
 }
 
 extension ArticleLocalDataSource: CommonArticleDataSource {
     
     func getTopArticles(country: String, category: String, complete: @escaping ([CommonArticle]) -> CommonStdlibUnit, fail: @escaping () -> CommonStdlibUnit) {
-        let query = QueryBuilder
-            .select(SelectResult.all())
-            .from(DataSource.database(database))
-            .orderBy(Ordering.property("createdAt").ascending())
-        do {
-            var articles: [ArticleResponse] = []
-            for row in try query.execute() {
-                if let dict = row.dictionary(forKey: ArticleLocalDataSource.databaseName) {
-                    let article = ArticleResponse(title: dict.string(forKey: ArticleResponse.CodingKeys.title.rawValue) ?? "",
-                                                  author: dict.string(forKey: ArticleResponse.CodingKeys.author.rawValue),
-                                                  url: dict.string(forKey: ArticleResponse.CodingKeys.url.rawValue) ?? "",
-                                                  imageUrl: dict.string(forKey: ArticleResponse.CodingKeys.imageUrl.rawValue))
-                    articles.append(article)
-                }
-            }
-            _ = complete(articles)
-        } catch {
-            _ = fail()
-        }
+        _ = complete([])
     }
     
     func save(topArticles: [CommonArticle], complete: @escaping () -> CommonStdlibUnit, fail: @escaping () -> CommonStdlibUnit) {
-        do {
-            // Delete old database and create new
-            try database.delete()
-            self.database = try Database(name: ArticleLocalDataSource.databaseName)
-            
-            try database.inBatch {
-                try topArticles.forEach { article in
-                    let newArticle = MutableDocument()
-                        .setString("[DB] \(article.title)", forKey: ArticleResponse.CodingKeys.title.rawValue)
-                        .setString(article.author, forKey: ArticleResponse.CodingKeys.author.rawValue)
-                        .setString(article.url, forKey: ArticleResponse.CodingKeys.url.rawValue)
-                        .setString(article.imageUrl, forKey: ArticleResponse.CodingKeys.imageUrl.rawValue)
-                        .setDate(Date(), forKey: "createdAt")
-                    
-                    try database.saveDocument(newArticle)
-                    print("ArticleLocalDataSource - Saved top article \(article.title)")
-                }
-                _ = complete()
-            }
-        } catch {
-            _ = fail()
-        }
+        _ = complete()
     }
 }
